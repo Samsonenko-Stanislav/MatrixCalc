b56c5bb889330f9e8c9ddcdadec82c74c67a3142
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-10-18 00:47:26 +0300

    Added determinantion

--- src/main/java/MatrixCalc/Determinant.java
+++ src/main/java/MatrixCalc/Determinant.java
@@ -1,8 +1,35 @@
 package MatrixCalc;
 
 public class Determinant {
-    public static double det(double a[][]){
-        double det = Double.parseDouble(null);
-        return det;
+    public static double det(double[][] a) {
+        double temp[][];
+        double det = 0;
+
+        if (a.length == 1) {
+            det = a[0][0];
+            return (det);
+        }
+
+        if (a.length == 2) {
+            det = ((a[0][0] * a[1][1]) - (a[0][1] * a[1][0]));
+            return (det);
+        }
+
+        for (int i = 0; i < a[0].length; i++) {
+            temp = new double[a.length][a[0].length];
+
+            for (int j = 1; j < a.length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    if (k < i) {
+                        temp[j][k] = a[j][k];
+                    } else if (k > i) {
+                        temp[j - 1][k - 1] = a[j][k];
+                    }
+                }
+            }
+
+            det += a[0][i] * Math.pow (-1, (double) i) * det(temp);
+        }
+        return (det);
     }
 }
